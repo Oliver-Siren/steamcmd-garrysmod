3ec590bc6e92d15c864cf3243ec0a6826cc8bb91
Konsta Toivonen <konsta.toivonen@myy.haaga-helia.fi>
2018-05-03 20:07:46 +0000

    modified run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ echo 'master: localhost' |sudo tee /etc/salt/minion
 echo "Installing SteamCMD"
 git clone https://github.com/a1702645/steamcmd-garrysmod
 echo "done"
-bash steamcmd-garrysmod/highstate.sh
+bash highstate.sh
 
