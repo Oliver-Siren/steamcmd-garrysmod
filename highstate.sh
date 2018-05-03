775f7daebfc5f64bda3ad25bf51e9d490cac2c69
Konsta Toivonen <konsta.toivonen@myy.haaga-helia.fi>
2018-05-03 16:42:49 +0000

    modified highstate.sh & run.sh

--- highstate.sh
+++ highstate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo salt-call --local --file-root srv/salt/ state.highstate --state-output applied -1 warning
+sudo salt-call --local --file-root srv/salt/ state.highstate
--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ sudo apt-get update
 sudo apt-get -y  install git salt-minion
 echo "Retrieving and Applying Settings"
 echo 'master: localhost' |sudo tee /etc/salt/minion
-
+echo "Installing SteamCMD"
 git clone https://github.com/a1702645/steamcmd-garrysmod
 cd steamcmd-garrysmod
 ./higstate.sh
+echo "Done"
